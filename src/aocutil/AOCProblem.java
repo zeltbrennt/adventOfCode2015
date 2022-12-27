9b75ce5531c0aeaf685b01a84453edcb7344ff17
zeltbrennt <zeltbrennt@gmail.com>
2022-12-27 10:56:29 +0100

    Create AOC Interface and InputReader

---
+++ src/aocutil/AOCProblem.java
@@ -0,0 +1,8 @@
+package aocutil;
+
+public interface AOCProblem {
+
+    int solvePart1();
+
+    int solvePart2();
+}
---
+++ src/aocutil/InputReader.java
@@ -0,0 +1,38 @@
+package aocutil;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class InputReader {
+
+    public static List<String> multipleLines(String source) {
+        BufferedReader file = null;
+        List<String> puzzle = new ArrayList<>();
+        String line = null;
+        try {
+            file = new BufferedReader(new FileReader(source));
+            while ((line = file.readLine()) != null) {
+                puzzle.add(line);
+            }
+        } catch (IOException e) {
+            System.out.println("File not found.");
+        }
+        return puzzle;
+    }
+
+    public static String singleLine(String source) {
+        BufferedReader file = null;
+        String puzzle = null;
+        try {
+            file = new BufferedReader(new FileReader("src/resources/01.txt"));
+            puzzle = file.readLine();
+        } catch (IOException e) {
+            System.out.println("File not found.");
+        }
+        return puzzle;
+    }
+
+}
