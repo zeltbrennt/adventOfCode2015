db920c6ca82035799a2f264065fac999319bca46
zeltbrennt <zeltbrennt@gmail.com>
2022-12-28 12:21:18 +0100

    Solve Day 04 Part 2

--- src/Day05.java
+++ src/Day05.java
@@ -10,10 +10,9 @@ public class Day05 implements AOCProblem {
     private final List<String> puzzle;
     public Day05(String inputFile) {
         this.puzzle = InputReader.multipleLines(inputFile);
-        //this.puzzle = List.of("ugknbfddgicrmopn", "aaa", "jchzalrnumimnmhp", "haegwjzuvuyypxyu", "dvszwmarrgswjxmb");
     }
 
-    private boolean naughtyOrNice(String str) {
+    private boolean threeRules(String str) {
         Pattern pVowels = Pattern.compile("[aeiou]");
         Pattern pDouble = Pattern.compile("(\\w)\\1+");
         Pattern pForbidden = Pattern.compile("ab|cd|pq|xy");
@@ -22,13 +21,27 @@ public class Day05 implements AOCProblem {
         Matcher mForbidden = pForbidden.matcher(str);
         return mVowels.results().count() >= 3 && mDouble.find() && !mForbidden.find();
     }
+
+    private boolean twoRules(String str) {
+        boolean r1 = false, r2 = false;
+        for (int i = 0, j = 2; j <= str.length(); i++, j++) {
+            Pattern pRepeat = Pattern.compile(str.substring(i, j));
+            Pattern pBetween = Pattern.compile(str.substring(i, j-1) + "." + str.substring(i, j-1));
+            Matcher mRepeat = pRepeat.matcher(str.substring(j));
+            Matcher mBetween = pBetween.matcher(str);
+            if (mRepeat.find()) r1 = true;
+            if (mBetween.find()) r2 = true;
+            if (r1 && r2) break;
+        }
+        return r1 && r2;
+    }
     @Override
     public int solvePart1() {
-        return puzzle.stream().mapToInt(str -> naughtyOrNice(str) ? 1 : 0).sum();
+        return puzzle.stream().mapToInt(str -> threeRules(str) ? 1 : 0).sum();
     }
 
     @Override
     public int solvePart2() {
-        return 0;
+        return puzzle.stream().mapToInt(str -> twoRules(str) ? 1 : 0).sum();
     }
 }
