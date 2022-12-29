a7e2c01d5fd4adf8b1aa8a22389745b20a96f22b
zeltbrennt <zeltbrennt@gmail.com>
2022-12-29 17:43:42 +0100

    Start day 07

---
+++ src/Day07.java
@@ -0,0 +1,22 @@
+import aocutil.AOCProblem;
+import aocutil.InputReader;
+
+import java.util.List;
+
+public class Day07 implements AOCProblem {
+
+    private final List<String> puzzle;
+
+    private Day07(String puzzleFile) {
+        this.puzzle = InputReader.multipleLines(puzzleFile);
+    }
+    @Override
+    public int solvePart1() {
+        return 0;
+    }
+
+    @Override
+    public int solvePart2() {
+        return 0;
+    }
+}
