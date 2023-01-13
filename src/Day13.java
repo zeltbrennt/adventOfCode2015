a72c8ddfdb9b33e97da3309f671b46c05d057aee
zeltbrennt <zeltbrennt@gmail.com>
2023-01-13 19:02:55 +0100

    Start Day13

---
+++ src/Day13.java
@@ -0,0 +1,23 @@
+import aocutil.AOCProblem;
+import aocutil.InputReader;
+
+import java.util.List;
+
+public class Day13 implements AOCProblem {
+
+    private final List<String> puzzle;
+
+    public Day13(String s) {
+        puzzle = InputReader.multipleLines(s);
+    }
+
+    @Override
+    public int solvePart1() {
+        return 0;
+    }
+
+    @Override
+    public int solvePart2() {
+        return 0;
+    }
+}
