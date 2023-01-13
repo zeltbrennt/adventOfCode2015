820a33346b25efef849c7f133322f44fd643a0c3
zeltbrennt <zeltbrennt@gmail.com>
2023-01-13 20:30:32 +0100

    Solve Day 14 Part 1

--- src/Day14.java
+++ src/Day14.java
@@ -8,7 +8,10 @@ import java.util.Set;
 
 public class Day14 implements AOCProblem {
 
+    private static final int RACE_DURATION = 2503;
     private final Set<Raindeer> racers = new HashSet<>();
+    private int maxDistance = 0;
+
     public Day14(String s) {
         List<String> puzzle = InputReader.multipleLines(s);
         for (String line : puzzle) {
@@ -19,7 +22,13 @@ public class Day14 implements AOCProblem {
 
     @Override
     public int solvePart1() {
-        return 0;
+        for (Raindeer raindeer : racers) {
+            int flyIntervals = RACE_DURATION / (raindeer.duration + raindeer.rest);
+            int distance = flyIntervals * raindeer.speed * raindeer.duration;
+            distance += Math.min(RACE_DURATION % (raindeer.duration + raindeer.rest), raindeer.duration) * raindeer.speed;
+            maxDistance = Math.max(distance, maxDistance);
+        }
+        return maxDistance;
     }
 
     @Override
