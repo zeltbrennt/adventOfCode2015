b0a00f190dcb30e8e72fb0f13a0d1ba5d30a11dd
zeltbrennt <zeltbrennt@gmail.com>
2023-01-14 19:13:41 +0100

    Solve Day 16 part 2

--- src/Day16.java
+++ src/Day16.java
@@ -15,11 +15,11 @@ public class Day16 implements AOCProblem {
         for (String line : InputReader.multipleLines(s)) {
             Integer[] prop = new Integer[10];
             for (int i = 0; i < properties.length; i++) {
-                int idx = line.indexOf(properties[i]);
-                if (idx != -1) {
-                    int idxStop = line.indexOf(',', idx);
+                int idxStart = line.indexOf(properties[i]);
+                if (idxStart != -1) {
+                    int idxStop = line.indexOf(',', idxStart);
                     idxStop = idxStop == -1 ? line.length() : idxStop;
-                    prop[i] = Integer.parseInt(line.substring(idx, idxStop).replaceAll("\\D", "").trim());
+                    prop[i] = Integer.parseInt(line.substring(idxStart, idxStop).replaceAll("\\D", ""));
                 }
             }
             aunts.add(new Sue(prop[0], prop[1], prop[2], prop[3], prop[4], prop[5], prop[6], prop[7], prop[8], prop[9]));
@@ -28,19 +28,21 @@ public class Day16 implements AOCProblem {
 
     @Override
     public int solvePart1() {
-        int x = -1;
         for (int i = 0; i < aunts.size(); i++) {
-            Sue aunt = aunts.get(i);
-            if (REFERENCE.equals(aunt)) {
-                System.out.println(aunt);
-                x = i;
+            if (REFERENCE.equals(aunts.get(i))) {
+                return i + 1;
             }
         }
-        return x+1;
+        return 0;
     }
 
     @Override
     public int solvePart2() {
+        for (int i = 0; i < aunts.size(); i++) {
+            if (REFERENCE.equals2(aunts.get(i))) {
+                return i + 1;
+            }
+        }
         return 0;
     }
 
@@ -48,16 +50,29 @@ public class Day16 implements AOCProblem {
                        Integer vizslas, Integer goldfish, Integer trees, Integer cars, Integer perfumes) {
 
         public boolean equals(Sue o) {
-            return (o.children == null || children.equals(o.children)) &&
-                    (o.cats == null || cats.equals(o.cats)) &&
-                    (o.samoyeds == null || samoyeds.equals(o.samoyeds)) &&
-                    (o.pomeranians == null || pomeranians.equals(o.pomeranians)) &&
-                    (o.akitas == null || akitas.equals(o.akitas)) &&
-                    (o.vizslas == null || vizslas.equals(o.vizslas)) &&
-                    (o.goldfish == null || goldfish.equals(o.goldfish)) &&
-                    (o.trees == null || trees.equals(o.trees)) &&
-                    (o.cars == null || cars.equals(o.cars)) &&
-                    (o.perfumes == null || perfumes.equals(o.perfumes));
+            return (o.children == null || children == o.children) &&
+                    (o.cats == null || cats == o.cats) &&
+                    (o.samoyeds == null || samoyeds == o.samoyeds) &&
+                    (o.pomeranians == null || pomeranians == o.pomeranians) &&
+                    (o.akitas == null || akitas == o.akitas) &&
+                    (o.vizslas == null || vizslas == o.vizslas) &&
+                    (o.goldfish == null || goldfish == o.goldfish) &&
+                    (o.trees == null || trees == o.trees) &&
+                    (o.cars == null || cars == o.cars) &&
+                    (o.perfumes == null || perfumes == o.perfumes);
+        }
+
+        public boolean equals2(Sue o) {
+            return (o.children == null || children == o.children) &&
+                    (o.cats == null || cats < o.cats) &&
+                    (o.samoyeds == null || samoyeds == o.samoyeds) &&
+                    (o.pomeranians == null || pomeranians > o.pomeranians) &&
+                    (o.akitas == null || akitas == o.akitas) &&
+                    (o.vizslas == null || vizslas == o.vizslas) &&
+                    (o.goldfish == null || goldfish > o.goldfish) &&
+                    (o.trees == null || trees < o.trees) &&
+                    (o.cars == null || cars == o.cars) &&
+                    (o.perfumes == null || perfumes == o.perfumes);
         }
     }
 }
