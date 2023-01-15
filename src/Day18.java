426424459e7b16fb77d328c6287abbf1d01a9d1d
zeltbrennt <zeltbrennt@gmail.com>
2023-01-15 20:05:39 +0100

    Start Day 18 with visualization

---
+++ src/Day18.java
@@ -0,0 +1,37 @@
+import aocutil.AOCProblem;
+import aocutil.InputReader;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Day18 implements AOCProblem {
+
+    private final int[] start;
+
+    public Day18(String s) {
+        List<Integer> status = new ArrayList<>();
+        for (String line : InputReader.multipleLines(s)) {
+            for (char c : line.toCharArray()) {
+                if (c == '#') status.add(1);
+                else status.add(0);
+            }
+        }
+        start = new int[status.size()];
+        for (int i = 0; i < status.size(); i++) {
+            start[i] = status.get(i);
+        }
+    }
+
+    public int[] exportStart() {
+        return start;
+    }
+    @Override
+    public int solvePart1() {
+        return 0;
+    }
+
+    @Override
+    public int solvePart2() {
+        return 0;
+    }
+}
