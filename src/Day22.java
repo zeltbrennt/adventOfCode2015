8e5bfb354e546eb07618b51d9077525072366c30
zeltbrennt <zeltbrennt@gmail.com>
2023-02-04 17:28:45 +0100

    Day 22: Solve part 1

--- src/Day22.java
+++ src/Day22.java
@@ -8,8 +8,10 @@ record Spell(spellName name, int MPCost) {
 }
 
 public class Day22 implements AOCProblem {
-    static final int PLAYER_HP = 100;
+    static final int PLAYER_HP = 50;
+    //static final int PLAYER_HP = 10;
     static final int PLAYER_MP = 500;
+    //static final int PLAYER_MP = 250;
     final Spell[] spells = new Spell[]{
             new Spell(spellName.POISON, 173),
             new Spell(spellName.MAGIC_MISSILE, 53),
@@ -21,70 +23,111 @@ public class Day22 implements AOCProblem {
     int bossDMG;
     int playerHP;
     int playerMP;
-    int shieldEffect;
-    int rechargeEffect;
-    int poisonEffect;
+    boolean difficult;
 
     public Day22(int bossHP, int bossDMG) {
         this.bossHP = bossHP;
         this.bossDMG = bossDMG;
         this.playerHP = PLAYER_HP;
         this.playerMP = PLAYER_MP;
-        this.poisonEffect = 0;
-        this.shieldEffect = 0;
-        this.rechargeEffect = 0;
     }
 
-    int takeTurn(int turn, Spell spell, int bossHP, int playerHP, int playerMP, int manaSpend, int poisonEffect, int rechargeEffect, int shieldEffect) {
-        if (playerMP < 0 || playerHP <= 0 || manaSpend > minMana) return minMana;
-        if (spell.name() == spellName.RECHARGE && rechargeEffect > 0) return minMana;
-        if (spell.name() == spellName.POISON && poisonEffect > 0) return minMana;
-        if (spell.name() == spellName.SHIELD && shieldEffect > 0) return minMana;
+    int takeTurn(int turn, Spell spell, int bossHP, int playerHP, int playerMP, int manaSpend, int poisonEffect, int rechargeEffect, int shieldEffect, String memo) {
         //player effect
-        shieldEffect = Math.max(0, shieldEffect - 1);
+        if (difficult) playerHP--;
+        if (playerHP <= 0) return Integer.MAX_VALUE;
+        memo += "-- Player turn " + turn + " --\n- Player has " + playerHP + " HP, " + (shieldEffect > 0 ? 7 : 0) + " armor, " + playerMP + " MP\n- Boss has " + bossHP + " HP\n";
+        if (shieldEffect > 0) {
+            shieldEffect--;
+            memo += "Shields timer is now " + shieldEffect + "\n";
+        }
         if (poisonEffect > 0) {
             bossHP -= 3;
             poisonEffect--;
+            memo += "Poison deals 3 damage, timer is now " + poisonEffect + "\n";
         }
         if (rechargeEffect > 0) {
             playerMP += 101;
             rechargeEffect--;
+            memo += "Recharge provides 101 mana, timer is now " + rechargeEffect + "\n";
         }
-        if (bossHP <= 0) return manaSpend;
-        if (turn % 2 == 0) {
-            //cast spell
-            playerMP -= spell.MPCost();
-            manaSpend += spell.MPCost();
-            switch (spell.name()) {
-                case MAGIC_MISSILE -> bossHP -= 4;
-                case DRAIN -> {
-                    playerHP += 2;
-                    bossHP -= 2;
-                }
-                case SHIELD -> shieldEffect = 6;
-                case POISON -> poisonEffect = 6;
-                case RECHARGE -> rechargeEffect = 5;
+        memo += "Player casts " + spell.name() + (spell.name() == spellName.MAGIC_MISSILE ? " deals 4 damage\n\n" : "\n\n");
+        //cast spell
+        playerMP -= spell.MPCost();
+        manaSpend += spell.MPCost();
+        switch (spell.name()) {
+            case MAGIC_MISSILE -> bossHP -= 4;
+            case DRAIN -> {
+                playerHP += 2;
+                bossHP -= 2;
             }
-        } else {
-            //boss effect and damage
-            playerHP -= shieldEffect > 0 ? Math.min(1, bossDMG - 7) : bossDMG;
+            case SHIELD -> shieldEffect = 6;
+            case POISON -> poisonEffect = 6;
+            case RECHARGE -> rechargeEffect = 5;
         }
-        for (Spell s : spells) {
-            minMana = Math.min(minMana, takeTurn(turn + 1, s, bossHP, playerHP, playerMP, manaSpend, poisonEffect, rechargeEffect, shieldEffect));
+        if (bossHP <= 0) {
+            System.out.println(memo + "boss dead \n===============================> " + manaSpend + " <===============================");
+            return manaSpend;
+        }
+        //boss effect and damage
+        turn++;
+        memo += "-- Boss turn " + turn + " --\n- Player has " + playerHP + " HP, " + (shieldEffect > 0 ? 7 : 0) + " armor, " + playerMP + " MP\n- Boss has " + bossHP + " HP\n";
+        if (shieldEffect > 0) {
+            shieldEffect--;
+            memo += "Shields timer is now " + shieldEffect + "\n";
+        }
+        if (poisonEffect > 0) {
+            bossHP -= 3;
+            poisonEffect--;
+            memo += "Poison deals 3 damage, timer is now " + poisonEffect + "\n";
+        }
+        if (rechargeEffect > 0) {
+            playerMP += 101;
+            rechargeEffect--;
+            memo += "Recharge provides 101 mana, timer is now " + rechargeEffect + "\n";
+        }
+        if (bossHP <= 0) {
+            System.out.println(memo + "boss dead \n===============================> " + manaSpend + " <===============================");
+            return manaSpend;
+        }
+        memo += "Boss deals 8 " + (shieldEffect > 0 ? "- 7 = 1 " : "") + "damage\n\n";
+        playerHP -= shieldEffect > 0 ? Math.min(1, bossDMG - 7) : bossDMG;
+        if (playerHP <= 0) return Integer.MAX_VALUE;
+        for (Spell nextSpell : spells) {
+            if (nextSpell.name() == spellName.RECHARGE && rechargeEffect > 0) continue;
+            if (nextSpell.name() == spellName.POISON && poisonEffect > 0) continue;
+            if (nextSpell.name() == spellName.SHIELD && shieldEffect > 0) continue;
+            if (nextSpell.MPCost() > playerMP) continue;
+            if (nextSpell.MPCost() + manaSpend > minMana) continue;
+            minMana = Math.min(minMana, takeTurn(turn + 1, nextSpell, bossHP, playerHP, playerMP, manaSpend, poisonEffect, rechargeEffect, shieldEffect, memo));
         }
         return minMana;
     }
 
+//    void breadthFirstPrint() {
+//        Deque<Spell> queue = new ArrayDeque<>(List.of(spells));
+//        while (!queue.isEmpty()) {
+//            Spell spell = queue.removeFirst();
+//            visited.add(node);
+//            System.out.printf("%s ", node); //do something /w the element after popping from queue
+//            for (T neighbor : graph.get(node)) {
+//                if (!visited.contains(neighbor)) queue.addLast(neighbor);
+//            }
+//        }
+//    }
+
     @Override
     public int solvePart1() {
         for (Spell spell : spells) {
-            minMana = takeTurn(0, spell, bossHP, playerHP, playerMP, 0, 0, 0, 0);
+            minMana = takeTurn(0, spell, bossHP, playerHP, playerMP, 0, 0, 0, 0, "");
         }
         return minMana;
     }
 
     @Override
     public int solvePart2() {
-        return 0;
+        minMana = Integer.MAX_VALUE;
+        difficult = true;
+        return solvePart1(); //1295 too high
     }
 }
