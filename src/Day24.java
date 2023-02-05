f65a8c1b4f9079ed254bb27f0ada66d1b140fb7d
zeltbrennt <zeltbrennt@gmail.com>
2023-02-05 09:00:44 +0100

    Start day 24

---
+++ src/Day24.java
@@ -0,0 +1,22 @@
+import aocutil.AOCProblem;
+import aocutil.InputReader;
+
+import java.util.List;
+
+public class Day24 implements AOCProblem {
+    private final List<String> packages;
+
+    public Day24(String s) {
+        this.packages = InputReader.multipleLines(s);
+    }
+
+    @Override
+    public int solvePart1() {
+        return 0;
+    }
+
+    @Override
+    public int solvePart2() {
+        return 0;
+    }
+}
