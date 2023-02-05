ab01bbf9837efa3adc0806e1869563ee8df4490b
zeltbrennt <zeltbrennt@gmail.com>
2023-02-05 09:50:02 +0100

    Day 24: calculate different target weights

--- src/Day24.java
+++ src/Day24.java
@@ -12,6 +12,24 @@ public class Day24 implements AOCProblem {
 
     @Override
     public int solvePart1() {
+        int[] r = new int[]{1, 2, 3, 4, 5, 7, 8, 9, 10, 11};
+        for (int i = 1; i < r.length; i++) {
+            for (int j = 1; j < 1 << r.length; j++) {
+                if (Integer.bitCount(j) == i) {
+                    String bits = String.format("%" + r.length + "s", Integer.toBinaryString(j)).replace(' ', '0');
+                    System.out.println(bits);
+                    int targetWeight = 0;
+                    System.out.print("[ ");
+                    for (int k = 0; k < bits.length(); k++) {
+                        if (bits.charAt(k) == '1') {
+                            System.out.printf("%02d ", r[k]);
+                            targetWeight += r[k];
+                        }
+                    }
+                    System.out.println("] = " + targetWeight);
+                }
+            }
+        }
         return 0;
     }
 
